715aecddc4afabed2a1b1f5a7dd3cafe739e09a0
VonNegriSteve <stevevonnegri@gmail.com>
2021-02-19 13:41:51 +0100

    Update Model.php

--- classes/Model.php
+++ classes/Model.php
@@ -23,94 +23,99 @@ class Model {
 		$this->_bdd = $bdd;
 	}
 
-    
+
+    public function setBddTableau($bdd) {
+		foreach ($bdd as $attribut => $valeur) {	
+			$method = 'set'.ucfirst($attribut); 
+			if(method_exists($this, $method)){ 
+				$this->$method($valeur);
+			}
+		}
+	}
+
 	public function getList(){
 		$lists = [];
 		$sql = $this->_bdd->query('SELECT * FROM '.$this->_table);
 		while($donnees = $sql->fetch(PDO::FETCH_ASSOC)){
 			$lists[] = new $this->_table($donnees);
 		}
-		
 		return $lists; 
 	} 
 
 
+	// recupère un seul personnage en fonction de l'id
 	/**
-    * fonction recuperant un élément d'une BDD selon un ID et un nom de colonne.
-    *
-    *@param      <int>       $id         l'id l'element
-    *@param      <string>   $colonne    nom de la colonne
-    *
-    *@return     <object>     retourne un objet avec les information de la BDD.
-    **/
-    public function getItem($id, $colonne = NULL){
-
-        if ($colonne == NULL) {
-            $colonne = $this->_cle;
-        }
-
-        $sql =$this->_bdd->query('SELECT * FROM '.$this->_table.' where '.$colonne.' = "'.$id.'"');
-        $donnees = $sql->fetch(PDO::FETCH_ASSOC);
-
-        //test si le SELECT a recupere des info ou non
-        if ($donnees == false) {
-            return false;
-        }
-        //retourne l'objet si la requete a recupere des info
-        return new $this->_table($donnees);
-    }
-
-
-	/**
-	* supprimer un élément de la BDD avec l'id
+	* fonction recuperant un élément d'une BDD selon un ID et un nom de colonne.
 	*
-	*@param      int   $id     l'id l'element
+	*@param      <int>   	$id     	l'id l'element
 	*@param      <string>   $colonne    nom de la colonne
+	*
+	*@return 	<object> 	retourne UN objet avec les information de la BDD.
+	**/
+	public function getItem($id, $colonne = NULL){
+
+		if ($colonne == NULL) {
+			$colonne = $this->_cle;
+		}
+
+		$sql =$this->_bdd->query('SELECT * FROM '.$this->_table.' where '.$colonne.' = "'.$id.'"');
+		$donnees = $sql->fetch(PDO::FETCH_ASSOC);
+
+		//test si le SELECT a recupere des info ou non
+		if ($donnees == false) {
+			return false;
+		}
+		//retourne l'objet si la requete a recupere des info
+		return new $this->_table($donnees);
+	}
+
+
+
+	/**
+	* fonction suppriment un élément d'une BDD selon un ID et un nom de colonne.
+	*
+	*@param      <int>   	$id     	l'id l'element
+	*@param      <string>   $colonne    nom de la colonne
+	*
 	**/
 	public function Delete($id, $colonne = NULL){
 
-        if ($colonne == NULL) {
-            $colonne = $this->_cle;
-        }
+		if ($colonne == NULL) {
+			$colonne = $this->_cle;
+		}
 
-        $this->_bdd->exec('DELETE FROM '.$this->_table.' WHERE '.$colonne.' = "'.$id.'"');
-    }
+		$this->_bdd->exec('DELETE FROM '.$this->_table.' WHERE '.$colonne.' = "'.$id.'"');
+	}
 
-	public function Add(array $data){
-
+	public function Add($objet){
 		$champs = '';
 		$valeurs = '';
-
-		foreach($data as $key => $value){
+		foreach($objet as $key => $value){
 			if($value){
-				$champs .= $key.' , ';
+				$champs .= substr($key,1).' , ';
 				$valeurs .= '"'.$value.'" , ';
 			}
 		}
-
 		$valeurs = substr($valeurs,0,-2);
 		$champs = substr($champs,0,-2);
 
 		$sql = $this->_bdd->prepare('INSERT INTO '.$this->_table.'('.$champs.') VALUES ('.$valeurs.')');
-
 		$sql->execute();
-
 	}
 
 	
 	public function Update(array $data, $id){
-
         $valeurs = '';
 		foreach($data as $key => $value) {
-			if($value) {
+			if(!is_null($value)) {
 				$valeurs .= $key.' = "'.$value.'" , ';
 			}
 		}
 		$valeurs = substr($valeurs,0,-2);
+
 		$sql = $this->_bdd->prepare('UPDATE '.$this->_table.' SET '.$valeurs.' WHERE id_'.$this->_table.' = '.$id);
 		$sql->execute();
 	}
-
 
 	/**
 	 * Compte le nombre d'entrer dans la table
